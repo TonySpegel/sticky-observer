1b815ff72c86f29052f631be38ee779378873261
Tony Spegel <TonySpegel@googlemail.com>
2017-11-09 08:17:39 +0100

    Dynamic Template Building and Styling

--- app/js/sticky-observer.js
+++ app/js/sticky-observer.js
@@ -7,15 +7,40 @@ window.onload = () => {
         'Library Lights',
     ];
 
+    const ARTICLE_SECTION = document.querySelector('#article-section');
+    const SECTION_LIST = document.querySelector('#section-list');
+    let arcticleSectionTemplate = '';
+    let sectionListTemplate = '';
+
     SECTIONS.forEach(section => {
-        console.log(normalizeSectionNames(section));
-    })
+        arcticleSectionTemplate += createSections(section);
+        sectionListTemplate += createSectionList(section);
+    });
+
+    ARTICLE_SECTION.insertAdjacentHTML('beforeend', arcticleSectionTemplate);
+    SECTION_LIST.insertAdjacentHTML('beforeend', sectionListTemplate);
 }
 
 /**
+ * Normalizes sections by replacing
+ * space with underscores and lowercase everything.
+ *
+ * Is used to create CSS-Class-Names and to set image paths as well.
  *
  * @param {string} section
+ * @returns {string}
  */
 function normalizeSectionNames(section) {
     return section.replace(/ /g, '_').toLowerCase();
+}
+
+/**
+ * Uses getComputedStyle to determine if an element is positioned 'sticky'
+ *
+ * @param {HTML} element
+ * @returns {boolean}
+ */
+function isElementSticky(element) {
+    const POSTION_VALUE = getComputedStyle(element).position;
+    return POSTION_VALUE.match('sticky') !== null;
 }
---
+++ app/js/template.js
@@ -0,0 +1,22 @@
+function createSections(section) {
+    const SECTION_NORMALIZED = normalizeSectionNames(section);
+    let template =
+        `<section>
+            <div class="sticky bg-color--${SECTION_NORMALIZED} shadow">
+                <img src="./img/${SECTION_NORMALIZED}.jpg" alt="${section}" />
+                <h2>${section}</h2>
+            </div>
+            <div>
+                Show pony churning anomalies we need to dialog around your choice of work attire, so feature creep, nor work. Cannibalize core competencies, for that jerk from finance really threw me under the bus, and pull in ten extra bodies to help roll the tortoise. Going forward sacred cow, so diversify kpis nor clear blue water so please advise soonest shotgun approach, nor draw a line in the sand. Put your feelers out we need more paper but rock Star/Ninja close the loop so please use "solutionise" instead of solution ideas! :). Churning anomalies collaboration through advanced technlogy. Powerpoint Bunny. Goalposts we just need to put these last issues to bed, so new economy or value-added action item reach out. We need more paper out of the loop globalize. Gain traction innovation is hot right now or hit the ground running, for baseline the procedure and samepage your department table the discussion let's unpack that later staff engagement.
+            </div>
+        </section>`;
+
+    return template;
+}
+
+function createSectionList(section) {
+    const SECTION_NORMALIZED = normalizeSectionNames(section);
+    let template = `<li>${section}</li>`;
+
+    return template;
+}
